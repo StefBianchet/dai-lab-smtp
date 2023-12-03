052c504b97a4886be174a7ead9d1b7db0c872042
michaelstrefeler <mstrefeler@me.com>
2023-12-03 18:03:18 +0100

    Added a cool header

--- src/main/java/ch/heig/dai/smtp/MailPrankster.java
+++ src/main/java/ch/heig/dai/smtp/MailPrankster.java
@@ -6,6 +6,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+/*
+$$\      $$\           $$\ $$\ $$$$$$$\                               $$\                   $$\
+$$$\    $$$ |          \__|$$ |$$  __$$\                              $$ |                  $$ |
+$$$$\  $$$$ | $$$$$$\  $$\ $$ |$$ |  $$ | $$$$$$\  $$$$$$\  $$$$$$$\  $$ |  $$\  $$$$$$$\ $$$$$$\    $$$$$$\   $$$$$$\
+$$\$$\$$ $$ | \____$$\ $$ |$$ |$$$$$$$  |$$  __$$\ \____$$\ $$  __$$\ $$ | $$  |$$  _____|\_$$  _|  $$  __$$\ $$  __$$\
+$$ \$$$  $$ | $$$$$$$ |$$ |$$ |$$  ____/ $$ |  \__|$$$$$$$ |$$ |  $$ |$$$$$$  / \$$$$$$\    $$ |    $$$$$$$$ |$$ |  \__|
+$$ |\$  /$$ |$$  __$$ |$$ |$$ |$$ |      $$ |     $$  __$$ |$$ |  $$ |$$  _$$<   \____$$\   $$ |$$\ $$   ____|$$ |
+$$ | \_/ $$ |\$$$$$$$ |$$ |$$ |$$ |      $$ |     \$$$$$$$ |$$ |  $$ |$$ | \$$\ $$$$$$$  |  \$$$$  |\$$$$$$$\ $$ |
+\__|     \__| \_______|\__|\__|\__|      \__|      \_______|\__|  \__|\__|  \__|\_______/    \____/  \_______|\__|
+*/
 
 public class MailPrankster {
     public static void main(String[] args) {
@@ -24,7 +34,6 @@ public class MailPrankster {
         ArrayList<Group> groups = mailManager.getGroups();
 
         mailManager.assignAMessagePerGroups();
-
         try (Socket socket = new Socket(configReader.getServerAddress(), configReader.getServerPort());
             var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
